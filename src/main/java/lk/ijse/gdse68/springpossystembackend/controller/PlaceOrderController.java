6dd8565b52b9ce97ae7f1fd76423be090c128f68
ApsaraWitharana <asachini095@gmail.com>
2024-10-14 14:50:00 +0530

    Add - loggers to order section

--- src/main/java/lk/ijse/gdse68/springpossystembackend/controller/PlaceOrderController.java
+++ src/main/java/lk/ijse/gdse68/springpossystembackend/controller/PlaceOrderController.java
@@ -3,9 +3,12 @@ package lk.ijse.gdse68.springpossystembackend.controller;
 import lk.ijse.gdse68.springpossystembackend.customerObj.PlaceOrderErrorResponse;
 import lk.ijse.gdse68.springpossystembackend.customerObj.PlaceOrderResponse;
 import lk.ijse.gdse68.springpossystembackend.dto.OrdersDTO;
+import lk.ijse.gdse68.springpossystembackend.entity.Customer;
 import lk.ijse.gdse68.springpossystembackend.exception.DataPersisFailedException;
 import lk.ijse.gdse68.springpossystembackend.service.PlaceOrderService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,11 +23,13 @@ import org.springframework.web.bind.annotation.*;
 public class PlaceOrderController {
     @Autowired
     PlaceOrderService placeOrderService;
+    Logger logger = LoggerFactory.getLogger(Customer.class);
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
      public ResponseEntity<String> placeOrder(@RequestBody OrdersDTO ordersDTO){
         try {
             placeOrderService.placeOrder(ordersDTO);
+            logger.info("Order Save Successfully!!");
             return new  ResponseEntity<>(HttpStatus.CREATED);
         }catch (DataPersisFailedException e){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
